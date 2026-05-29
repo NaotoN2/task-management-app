c9998b436119a686cc6ab421503b46945a6367ed
Naoto Noguchi <naoto2000x@gmail.com>
2026-05-29 20:40:07 +0900

    feat: improve login page UI

--- app/auth/login/LoginForm.tsx
+++ app/auth/login/LoginForm.tsx
@@ -18,11 +18,18 @@ export default function LoginForm() {
   };
 
   return (
-    <div>
-      {error && <p className="text-red-500">ログインに失敗しました。</p>}
-      <button className="cursor-pointer" onClick={handleLogin}>
-        Googleでログイン
-      </button>
-    </div>
+    <main className="flex min-h-screen items-center justify-center">
+      <div className="flex flex-col gap-4 items-center">
+        <h1 className="text-5xl font-bold">Task Manager</h1>
+
+        <p className="text-xl text-gray-500">タスク管理を効率的に</p>
+
+        {error && <p className="text-red-500">ログインに失敗しました。</p>}
+
+        <button className="cursor-pointer" onClick={handleLogin}>
+          <img src="/google-ctn.svg" alt="Googleで続ける"/>
+        </button>
+      </div>
+    </main>
   );
 }
